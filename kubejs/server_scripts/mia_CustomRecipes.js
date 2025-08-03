1d6fe8d3876857399b63cf6aa8cc04abff91c018
kilaXphantom <truckymick@live.com>
2025-08-03 19:29:35 +1000

    Added a recipe for the bell

--- kubejs/server_scripts/mia_CustomRecipes.js
+++ kubejs/server_scripts/mia_CustomRecipes.js
@@ -4,6 +4,18 @@ ServerEvents.recipes(event => {
 
     // Output, Input
     event.shapeless( 'minecraft:dirt', '9x minecraft:wheat_seeds' )
+
+    // Output, Input
+    event.shaped( 'minecraft:bell', [
+        'BBB',
+        'ACA',
+        'ADA'
+    ], {
+        A: 'minecraft:stone',
+        B: 'minecraft:stick',
+        C: 'minecraft:gold_block',
+        D: 'minecraft:gold_nugget'
+    })
 });
 
 console.info('Loaded custom recipes from mia_CustomRecipes. Thank you for choosing the Transcontinental Express!');
