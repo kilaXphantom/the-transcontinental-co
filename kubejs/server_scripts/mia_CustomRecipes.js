025001db7b77fe9cffab7a54fb834dc41e3f5586
kilaXphantom <truckymick@live.com>
2025-08-06 20:13:01 +1000

    Added better hanging sign recipes

--- kubejs/server_scripts/mia_CustomRecipes.js
+++ kubejs/server_scripts/mia_CustomRecipes.js
@@ -16,6 +16,73 @@ ServerEvents.recipes(event => {
         C: 'minecraft:gold_block',
         D: 'minecraft:gold_nugget'
     })
+
+    event.shaped( 'minecraft:oak_hanging_sign', [
+        'B B',
+        'AAA',
+        'AAA'
+    ], {
+        A: 'minecraft:oak_planks',
+        B: 'minecraft:chain'
+    })
+    event.shaped( 'minecraft:birch_hanging_sign', [
+        'B B',
+        'AAA',
+        'AAA'
+    ], {
+        A: 'minecraft:birch_planks',
+        B: 'minecraft:chain'
+    })
+    event.shaped( 'minecraft:spruce_hanging_sign', [
+        'B B',
+        'AAA',
+        'AAA'
+    ], {
+        A: 'minecraft:spruce_planks',
+        B: 'minecraft:chain'
+    })
+    event.shaped( 'minecraft:jungle_hanging_sign', [
+        'B B',
+        'AAA',
+        'AAA'
+    ], {
+        A: 'minecraft:jungle_planks',
+        B: 'minecraft:chain'
+    })
+    event.shaped( 'minecraft:acacia_hanging_sign', [
+        'B B',
+        'AAA',
+        'AAA'
+    ], {
+        A: 'minecraft:acacia_planks',
+        B: 'minecraft:chain'
+    })
+    event.shaped( 'minecraft:dark_oak_hanging_sign', [
+        'B B',
+        'AAA',
+        'AAA'
+    ], {
+        A: 'minecraft:dark_oak_planks',
+        B: 'minecraft:chain'
+    })
+    event.shaped( 'minecraft:mangrove_hanging_sign', [
+        'B B',
+        'AAA',
+        'AAA'
+    ], {
+        A: 'minecraft:mangrove_planks',
+        B: 'minecraft:chain'
+    })
+    event.shaped( 'minecraft:cherry_hanging_sign', [
+        'B B',
+        'AAA',
+        'AAA'
+    ], {
+        A: 'minecraft:cherry_planks',
+        B: 'minecraft:chain'
+    })
+
+    
 });
 
 console.info('Loaded custom recipes from mia_CustomRecipes. Thank you for choosing the Transcontinental Express!');
